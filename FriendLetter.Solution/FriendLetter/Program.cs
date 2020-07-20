1932510f841c33a8613ad134506692e92ec9f50c
Ian Gregg <iangregg188@gmail.com>
2020-07-20 09:35:17 -0700

    Establishing the Friend letter cs and laying out the groundwork.
    Co-authored-by: Ian Scott <chesnekov@gmail.com>

---
+++ FriendLetter.Solution/FriendLetter/Program.cs
@@ -0,0 +1,17 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FriendLetter
+{
+  public class Program
+  {
+    var host = new WebHostBuilder()
+        .UseKestrel();
+        .UseContentRoot(Directory.GetCurrentDirectory());
+        .UseIISIntegration();
+        .UseStartup<Startup>();
+        .Build();
+
+      host.Run();
+  }
+}
